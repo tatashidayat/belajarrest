996b5e37bdc01848ca92a19bbcbfba0a746f720d
tatashidayat <tatas.hidayat@gmail.com>
2018-11-16 13:48:39 +0700

    belajar-rest init

---
+++ src/test/java/com/emerio/rnd/belajarrest/BelajarRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.emerio.rnd.belajarrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BelajarRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
